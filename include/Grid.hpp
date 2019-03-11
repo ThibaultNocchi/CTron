4021c2add1e2d6e5e7c01cddbd2996890c83aab9
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-03-11 21:35:49 +0100

    Added some getters.

--- include/Grid.hpp
+++ include/Grid.hpp
@@ -1,9 +1,13 @@
 #pragma once
+#include <iostream>
 #include "Definitions.hpp"
 
 class Grid{
 	public:
-		Grid(int width=15, int height=15);
+		Grid(const int width=15, const int height=15);
+		int getWidth() const;
+		int getHeight() const;
+		std::pair<int, int> getSize() const;
 	private:
 		int width;
 		int height;
--- src/Grid.cpp
+++ src/Grid.cpp
@@ -1,6 +1,18 @@
 #include "Grid.hpp"
 
-Grid::Grid(int width, int height) {
+Grid::Grid(const int width, const int height) {
     this->width = width;
     this->height = height;
+}
+
+int Grid::getWidth() const{
+    return this->width;
+}
+
+int Grid::getHeight() const{
+    return this->height;
+}
+
+std::pair<int, int> Grid::getSize() const{
+    return std::pair<int, int>(this->width, this->height);
 }

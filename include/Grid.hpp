ab59fad9a6b7e260eb273ae81cfdab020c157e08
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-03-11 22:05:13 +0100

    Added grids methods.

--- include/Grid.hpp
+++ include/Grid.hpp
@@ -1,14 +1,24 @@
 #pragma once
 #include <iostream>
+#include <vector>
 #include "Definitions.hpp"
 
 class Grid{
 	public:
 		Grid(const int width=15, const int height=15);
+
 		int getWidth() const;
 		int getHeight() const;
 		std::pair<int, int> getSize() const;
+		int getIndex(const int x, const int y) const;
+		char getCell(const int x, const int y) const;
+
+		void setCell(const int x, const int y, char value);
+
+		void resetGrid();
+
 	private:
 		int width;
 		int height;
+		std::vector<char> cells;
 };
--- src/Grid.cpp
+++ src/Grid.cpp
@@ -3,6 +3,8 @@
 Grid::Grid(const int width, const int height) {
     this->width = width;
     this->height = height;
+    this->cells.resize(this->getWidth()*this->getHeight());
+    this->resetGrid();
 }
 
 int Grid::getWidth() const{
@@ -15,4 +17,24 @@ int Grid::getHeight() const{
 
 std::pair<int, int> Grid::getSize() const{
     return std::pair<int, int>(this->width, this->height);
+}
+
+int Grid::getIndex(const int x, const int y) const{
+    return x*this->getWidth() + this->getHeight();
+}
+
+char Grid::getCell(const int x, const int y) const{
+    return this->cells[this->getIndex(x, y)];
+}
+
+void Grid::setCell(const int x, const int y, char value){
+    this->cells[this->getIndex(x, y)] = value;
+}
+
+void Grid::resetGrid(){
+    for(int x = 0; x < this->getWidth(); ++x){
+        for(int y = 0; y < this->getHeight(); ++y){
+            this->setCell(x, y, 0);
+        }
+    }
 }

26682e3fc16b517c5a7b28a0348c028780cc9297
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-03-17 13:11:44 +0100

    Added getters to gridstate.

--- include/GridState.hpp
+++ include/GridState.hpp
@@ -7,7 +7,8 @@
 class GridState{
 	public:
 		GridState(const std::vector<Snake>& vSnakes, const COORDS& coor);
-
+		const std::vector<Snake>& getSnakes() const;
+		const COORDS& getCoords() const;
 	private:
 		std::pair<std::vector<Snake>, COORDS> pair;
 };
--- src/GridState.cpp
+++ src/GridState.cpp
@@ -4,3 +4,11 @@ GridState::GridState(const std::vector<Snake>& vSnakes, const COORDS& coor){
 	this->pair.first = vSnakes;
 	this->pair.second = coor;
 }
+
+const std::vector<Snake>& GridState::getSnakes() const{
+	return this->pair.first;
+}
+
+const COORDS& GridState::getCoords() const{
+	return this->pair.second;
+}

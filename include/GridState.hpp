b9a35453c3b3ac5fb625b6674c0417d3da5404b4
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-03-17 13:16:23 +0100

    Added equal operator.

--- include/GridState.hpp
+++ include/GridState.hpp
@@ -8,7 +8,8 @@ class GridState{
 	public:
 		GridState(const std::vector<Snake>& vSnakes, const COORDS& coor);
 		const std::vector<Snake>& getSnakes() const;
-		const COORDS& getCoords() const;
+		const COORDS& getBonus() const;
+		bool operator==(const GridState& other) const;
 	private:
 		std::pair<std::vector<Snake>, COORDS> pair;
 };
--- src/GridState.cpp
+++ src/GridState.cpp
@@ -9,6 +9,39 @@ const std::vector<Snake>& GridState::getSnakes() const{
 	return this->pair.first;
 }
 
-const COORDS& GridState::getCoords() const{
+const COORDS& GridState::getBonus() const{
 	return this->pair.second;
+}
+
+bool GridState::operator==(const GridState& other) const{
+
+	if(this->pair.first.size() != other.getSnakes().size()) return false;
+
+    auto bonus1 = this->pair.second;
+    auto bonus2 = other.getBonus();
+
+    if(bonus1.first != bonus2.first) return false;
+    if(bonus1.second != bonus2.second) return false;
+
+    auto snakes1 = this->getSnakes();
+    auto snakes2 = other.getSnakes();
+
+    for(size_t i = 0; i < snakes1.size(); ++i){
+        if(snakes1[i].getAlive() != snakes2[i].getAlive()) return false;
+
+        if(snakes1[i].getCurrentLength() != snakes2[i].getCurrentLength()) return false;
+
+        auto body1 = snakes1[i].getBody();
+        auto body2 = snakes2[i].getBody();
+
+        for(size_t j = 0; j < body1.size(); ++j){
+            auto pos1 = body1[j];
+            auto pos2 = body2[j];
+            if(pos1.first != pos2.first) return false;
+            if(pos1.second != pos2.second) return false;
+        }
+    }
+
+    return true;
+	
 }

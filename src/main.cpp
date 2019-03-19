1acaab4c6b8d852d4498ff5f0d7cf6cebde1ff90
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-03-19 22:05:42 +0100

    Prepped main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <omp.h>
+#include <unordered_map>
 #include "Definitions.hpp"
 #include "Grid.hpp"
 #include "Snake.hpp"
@@ -13,7 +14,10 @@ int main(int argc, char* argv[]){
 
 	grid.displayGridBasic();
 
+	std::unordered_map<u_int64_t, int> mymap;
+
 	double begin = omp_get_wtime();
+	mymap[grid.getHash()]++;
 	
 	for(int i = 0; i < 100000; ++i){
 		
@@ -37,11 +41,16 @@ int main(int argc, char* argv[]){
 		}
 
 		grid.moveSnakes();
+		mymap[grid.getHash()]++;
 
 	}
 
-	// grid.displayGridBasic();
+	grid.displayGridBasic();
 	std::cout << omp_get_wtime() - begin << std::endl;
+
+	// for(auto el : mymap){
+	// 	if(el.second > 1) std::cout << el.second << " times: " << el.first << std::endl;
+	// }
 
 	return 0;
 }

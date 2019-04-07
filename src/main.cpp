1820c7b74804fb433a0effa09ca75c5d5ce10f77
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-04-07 15:02:59 +0200

    Added the possibility to display 1 game||run many

--- src/main.cpp
+++ src/main.cpp
@@ -7,91 +7,96 @@
 
 int main(int argc, char *argv[]) {
 
-  // auto grid = Grid(10, 10);
-  // grid.addWall(std::pair<COORDTYPE, COORDTYPE>(2,2), std::pair<COORDTYPE,COORDTYPE>(4,4));
-  // auto player = grid.addSnake(3, 1); grid.addSnake(3, 1);
+  if(argc <= 1 || std::stoi(argv[1]) <= 1){
 
-  // auto brain0 = BrainMCTS(player, grid);
-
-  // grid.displayGridBasic();
-
-  // int ending;
-  // double begin = omp_get_wtime();
-
-  // int rounds = 0;
-
-  // for(int i = 0; i < 100000; ++i){
-
-  // 	for(int j = 0; j < MC_ITERATIONS; ++j){
-  // 		brain0.explore(grid);
-  // 	}
-
-  // 	grid.setNewRandomDirectionsExcept(0, true);
-  // 	grid.setDirection(0, brain0.selectDirection(grid));
-  // 	ending = grid.moveSnakes();
-  // 	if(ending >= -1) break;
-
-  // 	grid.displayGridBasic();
-  // 	// sleep(1);
-
-  // 	++rounds;
-
-  // }
-
-  // std::cout << omp_get_wtime() - begin << std::endl;
-
-  // grid.displayGridBasic();
-  // switch(ending){
-  // 	case -1:
-  // 		std::cout << "Every snake died." << std::endl;
-  // 		break;
-  // 	case -2:
-  // 		std::cout << "No winner." << std::endl;
-  // 		break;
-  // 	default:
-  // 		std::cout << "Snake n°" << ending << " won!" << std::endl;
-  // }
-
-  // std::cout << rounds << " rounds." << std::endl;
-
-  int victories = 0;
-  std::vector<int> roundsNbr;
-
-  for (int game = 0; game < 10; ++game) {
-
-    int rounds = 0;
     auto grid = Grid(10, 10);
-    // grid.addWall(std::pair<COORDTYPE, COORDTYPE>(2, 2), std::pair<COORDTYPE, COORDTYPE>(4, 4));
-    auto player = grid.addSnake(3, 1);
-    grid.addSnake(3, 1);
+    grid.addWall(std::pair<COORDTYPE, COORDTYPE>(2,2), std::pair<COORDTYPE,COORDTYPE>(4,4));
+    auto player = grid.addSnake(3, 1); grid.addSnake(3, 1);
 
     auto brain0 = BrainMCTS(player, grid);
 
-    int ending = -2;
+    grid.displayGridBasic();
 
-    while (ending < -1) {
+    int ending;
+    double begin = omp_get_wtime();
 
-      for (int j = 0; j < MC_ITERATIONS; ++j) {
+    int rounds = 0;
+
+    for(int i = 0; i < 100000; ++i){
+
+      for(int j = 0; j < MC_ITERATIONS; ++j){
         brain0.explore(grid);
       }
 
       grid.setNewRandomDirectionsExcept(0, true);
       grid.setDirection(0, brain0.selectDirection(grid));
       ending = grid.moveSnakes();
+      if(ending >= -1) break;
+
+      grid.displayGridBasic();
+      // sleep(1);
+
       ++rounds;
+
     }
 
-	if(ending == 0) ++victories;
-  std::cout << "Round " << game + 1 << " ended in " << rounds << " rounds! Score: " << victories << "/" << game + 1 << std::endl;
-  roundsNbr.push_back(rounds);
+    std::cout << omp_get_wtime() - begin << std::endl;
+
+    grid.displayGridBasic();
+    switch(ending){
+      case -1:
+        std::cout << "Every snake died." << std::endl;
+        break;
+      case -2:
+        std::cout << "No winner." << std::endl;
+        break;
+      default:
+        std::cout << "Snake n°" << ending << " won!" << std::endl;
+    }
+
+    std::cout << rounds << " rounds." << std::endl;
+
+  }else{
+
+    int victories = 0;
+    std::vector<int> roundsNbr;
+
+    for (int game = 0; game < std::stoi(argv[1]); ++game) {
+
+      int rounds = 0;
+      auto grid = Grid(10, 10);
+      // grid.addWall(std::pair<COORDTYPE, COORDTYPE>(2, 2), std::pair<COORDTYPE, COORDTYPE>(4, 4));
+      auto player = grid.addSnake(3, 1);
+      grid.addSnake(3, 1);
+
+      auto brain0 = BrainMCTS(player, grid);
+
+      int ending = -2;
+
+      while (ending < -1) {
+
+        for (int j = 0; j < MC_ITERATIONS; ++j) {
+          brain0.explore(grid);
+        }
+
+        grid.setNewRandomDirectionsExcept(0, true);
+        grid.setDirection(0, brain0.selectDirection(grid));
+        ending = grid.moveSnakes();
+        ++rounds;
+      }
+
+    if(ending == 0) ++victories;
+    std::cout << "Round " << game + 1 << " ended in " << rounds << " rounds! Score: " << victories << "/" << game + 1 << std::endl;
+    roundsNbr.push_back(rounds);
+
+    }
+
+    double meanRounds = 0;
+    for(auto rounds : roundsNbr) meanRounds += rounds;
+    meanRounds /= roundsNbr.size();
+
+    std::cout << victories << " victories for our snake for a mean number of rounds of " << meanRounds << "!" << std::endl;
 
   }
-
-  double meanRounds = 0;
-  for(auto rounds : roundsNbr) meanRounds += rounds;
-  meanRounds /= roundsNbr.size();
-
-  std::cout << victories << " victories for our snake for a mean number of rounds of " << meanRounds << "!" << std::endl;
-
   return 0;
 }
